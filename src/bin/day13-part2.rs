0ba536a00ba953b8c1745da3a7f6f3604a6f33ac
Brannegan <zapp.brannegan@gmail.com>
2023-12-19 09:55:02 +0700

    Day 13. Mirrors

--- src/bin/day13-part2.rs
+++ src/bin/day13-part2.rs
@@ -1,9 +1,8 @@
 use std::fs::read_to_string;
-use std::ops::Range;
 
 use itertools::Itertools;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Pattern {
     rows: Vec<String>,
     cols: Vec<String>,
@@ -31,31 +30,34 @@ fn parse(input: &str) -> Vec<Pattern> {
         .map(Pattern::new)
         .collect()
 }
-
 fn mirror_pos(lines: &[String]) -> Option<usize> {
     lines
         .iter()
         .enumerate()
         .tuple_windows()
-        .filter(|((_, a), (_, b))| a == b)
+        .filter(|((_, a), (_, b))| {
+            a == b || a.chars().zip(b.chars()).filter(|(a, b)| a != b).count() <= 1
+        })
         .find_map(|((a_i, _), (b_i, _))| {
             let top = &lines[0..=a_i];
             let btm = &lines[b_i..];
-            top.iter()
+            (top.iter()
                 .rev()
                 .flat_map(|line| line.chars())
                 .zip(btm.iter().flat_map(|line| line.chars()))
-                .all(|(a, b)| a == b)
+                .filter(|(a, b)| a != b)
+                .count()
+                == 1)
                 .then_some(a_i + 1)
         })
 }
-fn part1(patterns: &[Pattern]) -> usize {
+fn part2(patterns: &[Pattern]) -> usize {
     patterns
         .iter()
-        .map(|pat| {
-            mirror_pos(&pat.rows)
+        .map(|pattern| {
+            mirror_pos(&pattern.rows)
                 .map(|left| left * 100)
-                .or_else(|| mirror_pos(&pat.cols))
+                .or_else(|| mirror_pos(&pattern.cols))
                 .unwrap_or(0)
         })
         .sum()
@@ -64,7 +66,7 @@ fn part1(patterns: &[Pattern]) -> usize {
 fn main() {
     let input = read_to_string("inputs/day13-input1.txt").unwrap();
     let patterns = parse(&input);
-    let answer = part1(&patterns);
+    let answer = part2(&patterns);
     println!("answer is: {answer}");
 }
 #[cfg(test)]
@@ -102,13 +104,13 @@ mod tests {
     fn mirror_test() {
         let patterns = parse(INPUT.trim());
 
-        assert_eq!(mirror_pos(&patterns[0].cols), Some(5));
-        assert_eq!(mirror_pos(&patterns[1].rows), Some(4));
+        assert_eq!(mirror_pos(&patterns[0].rows), Some(3));
+        assert_eq!(mirror_pos(&patterns[1].rows), Some(1));
     }
 
     #[test]
-    fn part1_test() {
+    fn part2_test() {
         let patterns = parse(INPUT.trim());
-        assert_eq!(part1(&patterns), 400);
+        assert_eq!(part2(&patterns), 402);
     }
 }
